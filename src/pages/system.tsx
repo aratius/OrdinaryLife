64c2d4b963644befc8618553b6a673255ae21b5e
aratius <arata1129matsu@icloud.com>
2022-11-20 17:43:49 +0900

    indexにも

--- src/pages/system.tsx
+++ src/pages/system.tsx
@@ -6,23 +6,30 @@ import WebGLMain from "src/lib/webgl/projects/paper/main";
 import styles from "src/styles/index.module.scss"
 
 interface Props {}
-interface State {}
+interface State {
+    bg: boolean
+}
 
 export default class Index extends Component {
 
-    public state: State = {}
+    public state: State = {
+        bg: false
+    }
 	private _webgl: WebGLMain | null = null
+    private _container: HTMLDivElement|null = null
+    private _lastProgress: number = 0
+
     constructor(props: Props) {
         super(props)
-        this.state = {}
     }
 
     public componentDidMount(): void {
+        window.addEventListener("scroll", this._onScroll)
     }
 
 	componentWillUnmount(): void {
 		// this._webgl?.deInit()
-		this._webgl = null
+		// this._webgl = null
 	}
 
 	private _onRefCanvas = (node: HTMLCanvasElement): void => {
@@ -31,9 +38,39 @@ export default class Index extends Component {
 		this._webgl.init()
 	}
 
+    private _onScroll = (e: Event) => {
+
+        if(!this._webgl || !this._container) return
+        const rect = this._container?.getBoundingClientRect()
+        const progress = -(rect!.top) / (rect!.height - innerHeight)
+
+        if(
+            this._lastProgress < .05 && progress >= .05 ||
+            this._lastProgress > .95 && progress <= .95
+
+        ) {
+            this._webgl!.spread()
+            this.setState({bg: true})
+        } else if(
+            this._lastProgress < .95 && progress >= .95 ||
+            this._lastProgress > .05 && progress <= .05
+        ) {
+            this._webgl!.fold()
+            this.setState({bg: false})
+        }
+
+        this._webgl.setCaos(progress)
+
+        this._lastProgress = progress
+    }
+
     public render(): ReactElement {
+        const { bg } = this.state
         return (
-            <div className={`${styles.container} ${styles.bg_black}`}>
+            <div
+                className={`${styles.container} ${bg ? styles.bg_black : ""}`}
+                ref={node => this._container = node}
+            >
                 <div style={{height:"100vh"}}></div>
                 <ScrollContents />
                 <Link href="/test" className={styles.link}>{"> 寿命生成デモ"}</Link>
