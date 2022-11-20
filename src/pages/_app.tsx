79d33c80fb0076a8a9e6485b2ee0e6df6ac16472
aratius <arata1129matsu@icloud.com>
2022-11-20 19:57:02 +0900

    OG修正

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,9 +7,9 @@ export default function App({ Component, pageProps }: AppProps) {
     <Head>
       <meta property="og:url" content="https://existence.aualrxse.com/" />
       <meta property="og:type" content="website" />
-      <meta property="og:title" content="Existence - arata matsumoto" />
-      <meta property="og:description" content="Site of an installation created by arata matsumoto" />
-      <meta property="og:site_name" content="eden" />
+      <meta property="og:title" content="What is the 'existence' ?" />
+      <meta property="og:description" content="A site of an installation created by arata matsumoto" />
+      <meta property="og:site_name" content="Existence - arata matsumoto" />
       <meta property="og:image" key="ogImage" content="https://existence.aualrxse.com/images/og.png" />
       <meta name="twitter:card" key="twitterCard" content="summary_large_image" />
       <meta name="twitter:site" content="@aualrxse" />
