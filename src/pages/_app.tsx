69ed4ef66986032b174596059927de90c5d2d128
aratius <arata1129matsu@icloud.com>
2022-11-20 19:49:31 +0900

    OG追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <>
+    <Head>
+      <meta property="og:url" content="https://existence.aualrxse.com/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Existence - arata matsumoto" />
+      <meta property="og:description" content="Site of an installation created by arata matsumoto" />
+      <meta property="og:site_name" content="eden" />
+      <meta property="og:image" key="ogImage" content="https://existence.aualrxse.com/images/og.png" />
+      <meta name="twitter:card" key="twitterCard" content="summary_large_image" />
+      <meta name="twitter:site" content="@aualrxse" />
+    </Head>
+    <Component {...pageProps} />
+  </>
 }
